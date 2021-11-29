a9058f445dd550942a18867bdfcd88a5f6a04762
ver0cr <veronicacr140@gmail.com>
2021-11-28 20:18:06 -0500

    commit

--- Proyecto Encriptado BASE 64/assets/js/encriptado.js
+++ Proyecto Encriptado BASE 64/assets/js/encriptado.js
@@ -16,7 +16,7 @@ for (let index = 48; index <= 57; index++) {
 base64.push("+");
 base64.push("/");
 
-function encriptar(elemento) {
+function encriptar() {
     var palabra = document.getElementsByTagName("input")[0].value;
     var base64Frase = "";
 
@@ -29,7 +29,7 @@ function encriptar(elemento) {
             let asciiCode = subPalabra.charCodeAt(j);
             binChar = asciiCode.toString(2);
             nCerosIzquierda = 8 - (binChar.length % 8); // AGREGADO: Algunas veces el número binario tiene 6 dígitos y otras veces 7
-            
+
             let cerosIzquierda = "";
             for (let k = 0; k < nCerosIzquierda; k++) {
                 cerosIzquierda += "0";
@@ -52,7 +52,7 @@ function encriptar(elemento) {
                 newAscii = parseInt(bytes6, 2);
                 newbase64 += base64[newAscii];
             }
-            
+
             let cerosDerecha = 6 - (binString.length % 6); // CORREGIDO
             let subBinString = "00" + binString.slice((Math.floor(binString.length / 6) * 6)); // CORREGIDO
             for (let k = 0; k < cerosDerecha; k++) {
@@ -69,8 +69,77 @@ function encriptar(elemento) {
     document.getElementById("resultado").innerHTML = base64Frase;
 }
 
-function desencriptar(elemento){
-    var palabra = document.getElementsByTagName("input")[0].value;
-    var palabra_descodificada = atob(palabra);
-    document.getElementById("resultado").innerHTML = palabra_descodificada;
+
+//desencriptar
+
+
+function desencriptar() {
+    var base64Frase = document.getElementsByTagName("input")[0].value;
+    var palabra_desencriptada = "";
+    var indice64 = -1;
+    var numbinarios = "";
+    var binario = "";
+    var binString = "";
+    var nceros = -1;
+    var ceros = "";
+    var iguales = [];
+    var niguales = 0;
+
+    //1. Buscamos las referencias decimales de la tabla de Base64
+
+    for (let i = 0; i < base64Frase.length; i++) {
+        if (i < base64Frase.length - 1) {
+            if (base64Frase[i + 1] == "=") {
+
+                iguales = base64Frase.filter(e => e == "=");
+                niguales = iguales.length;
+                indice64 = base64.findIndex(base64Frase[i]);
+                binario = indice64.toString(2); //0
+
+                nceros = 8 - (binario.length % 8); // AGREGADO: Algunas veces el número binario tiene 6 dígitos y otras veces 7
+
+                let ceros = "";
+                for (let m = 0; m < nceros; m++) {
+                    ceros += "0";
+                }
+
+                binario = ceros + binario;
+
+                binario = binario.slice(2, 8 - 2 * niguales);
+
+                binString += binario;
+
+                break//que pasa si es igual a =, los ceros van a la izquierda
+
+            }
+        }
+        //18 0 ==
+        indice64 = base64.findIndex(base64Frase[i]);
+        binario = indice64.toString(2); //10010
+        //1
+        nceros = 6 - (binario.length % 6); // AGREGADO: Algunas veces el número binario tiene 6 dígitos y otras veces 7
+
+        let ceros = "";
+        for (let m = 0; m < nceros; m++) {
+            ceros += "0";
+        }
+
+        // estoy segura
+        binString += ceros + binario; //010010
+
+    }
+    var subBin = "";
+    var ascii = -1;
+    
+
+    for (let h = 0; h < binString.length; h += 8) {
+        subBin = binString.slice(h, h + 8);
+
+         ascii =  parseInt(subBin, 2); 
+
+         palabra_desencriptada += String.fromCharCode(ascii);
+    }
+
+
+    document.getElementById("resultado").innerHTML = palabra_desencriptada;
 }
